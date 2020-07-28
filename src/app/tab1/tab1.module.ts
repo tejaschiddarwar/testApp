0770fc890a4e62fe91361eab53528b57ebff6cfe
Tejas Chiddarwar <tejaschiddarwar@gmail.com>
2020-07-28 23:02:42 +0530

    my projects

---
+++ src/app/tab1/tab1.module.ts
@@ -0,0 +1,38 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Tab1Page } from './tab1.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    Tab1PageRoutingModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 40,
+      "space": -5,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#4882c2",
+      "outerStrokeGradientStopColor": "#4882c2",
+      "innerStrokeColor": "#4882c2",
+      "innerStrokeWidth": 10,
+      "title": "83",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": false,
+      "startFromZero": false
+    })
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
